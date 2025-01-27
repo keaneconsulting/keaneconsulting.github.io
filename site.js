c6d80a31706dad356583df207713931b0913b467
keaneconsulting <127808194+keaneconsulting@users.noreply.github.com>
2025-01-27 17:15:42 -0600

    added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,17 @@
-console.log('Config Successful!');
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+let greeting;
+if (isMorning) {
+    greeting = "Good Morning!";
+} else if (isAfternoon) {
+    greeting = "Good Afternoon!";
+} else if (isEvening) {
+    greeting = "Good Evening!";
+}
+
+const welcome = document.querySelector('#welcome');
+welcome.textContent = `${greeting} Welcome to Evan's Cool Site!`;
