f27d499df543226d6604b21b7dcac09c32963804
keaneconsulting <127808194+keaneconsulting@users.noreply.github.com>
2025-02-18 10:40:07 -0600

    week 6 assignment 5

--- site.js
+++ site.js
@@ -20,3 +20,40 @@ welcome.textContent = `${greeting} Welcome to Evan's Cool Site!`;
 localStorage.setItem("It's a secret to everybody.", "Secret message: World of Warcraft reference?");
 const secretMessage = localStorage.getItem("It's a secret to everybody.");
 console.log(secretMessage);
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
